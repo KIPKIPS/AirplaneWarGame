17aef7c682ddd2a9d0878073a7426ea221d8273a
RECNEPS-KIPS <2432719656@qq.com>
2020-02-09 13:43:35 +0800

    实现背景循环

---
+++ Assets/Scripts/BackgroundTransform.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundTransform : MonoBehaviour {
+    public float moveSpeed;
+    // Start is called before the first frame update
+    void Start() {
+        moveSpeed = 8;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Translate(Vector3.down*moveSpeed*Time.deltaTime);
+        if (transform.position.y<=-8.52f) {
+            transform.position += new Vector3(0, 17.04f, 0);
+        }
+    }
+}

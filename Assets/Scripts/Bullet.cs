3241c94a87304f8322e7d1f8dc7b8b2b4e1be14a
RECNEPS-KIPS <2432719656@qq.com>
2020-02-10 16:50:08 +0800

    实现发射子弹功能

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+    public float speed;
+    // Start is called before the first frame update
+    void Start() {
+        speed = 4;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.up*speed*Time.deltaTime);
+        //超出边界 销毁游戏物体
+        if (transform.position.y>4.4f) {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour {
+    public float fireSpeed;
+
+    public GameObject bullet;
+    // Start is called before the first frame update
+    void Start() {
+        fireSpeed = 0.2f;
+        OpenFire();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Fire() {
+        //实例化一个子弹,位置为当前游戏物体位置,旋转角度为默认Quaternion.identity
+        GameObject.Instantiate(bullet,transform.position,Quaternion.identity);
+    }
+
+    public void OpenFire() {
+        InvokeRepeating("Fire",1,fireSpeed);
+    }
+}

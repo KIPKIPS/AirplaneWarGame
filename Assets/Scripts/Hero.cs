63742249b89b9ef2050fd0a54a0041447a5580a8
RECNEPS-KIPS <2432719656@qq.com>
2020-02-10 19:08:42 +0800

    实现防止玩家越界功能

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -4,6 +4,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Hero : MonoBehaviour {
+    public float moveSpeed;
+    //输入信号值
+    public float Dup;
+    public float Dright;
+    //平滑输入值
+    private float targetDup;
+    private float targetDright;
+    private float velocityDup;
+    private float velocityDright;
+
+    public string keyUp = "w";
+    public string keyDown = "s";
+    public string keyLeft = "a";
+    public string keyRight = "d";
+
     public bool canPlayAnim;//是否播放动画
     public int fps = 10;//fps
     public float timer=0;
@@ -15,6 +30,7 @@ public class Hero : MonoBehaviour {
     void Start() {
         canPlayAnim = true;
         sr = GetComponent<SpriteRenderer>();
+        moveSpeed = 7;
     }
 
     // Update is called once per frame
@@ -26,6 +42,19 @@ public class Hero : MonoBehaviour {
             //frameIndex %= 2;
             sr.sprite = sprites[frameIndex];
         }
+        targetDup = (Input.GetKey(keyUp) ? 1.0f : 0) - (Input.GetKey(keyDown) ? 1.0f : 0);
+        targetDright = (Input.GetKey(keyRight) ? 1.0f : 0) - (Input.GetKey(keyLeft) ? 1.0f : 0);
+        //对输入值进行插值计算
+        Dup = Mathf.SmoothDamp(Dup, targetDup, ref velocityDup, 0.1f);//x AD
+        Dright = Mathf.SmoothDamp(Dright, targetDright, ref velocityDright, 0.1f);//y WS
+
+        transform.Translate(new Vector3(Dright,Dup,transform.position.z)*moveSpeed*Time.deltaTime);
+        //防止越界
+        float x=Mathf.Clamp(transform.position.x, -1.8f, 1.8f);
+        float y = Mathf.Clamp(transform.position.y, -3.66f, 3.66f);
+
+        transform.position = new Vector3(x, y,transform.position.z);
+
     }
 
 }

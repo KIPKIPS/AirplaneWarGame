7d9cb251056644568bbbad68a01f3d28a8eddc99
RECNEPS-KIPS <2432719656@qq.com>
2020-02-10 16:51:23 +0800

    实现奖励移动

---
+++ Assets/Scripts/Award.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Award : MonoBehaviour {
+    public int type;//0 gun / 1 boom
+    public float speed;
+    // Start is called before the first frame update
+    void Start() {
+        speed = 1.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //移动
+        transform.Translate(Vector3.down*speed*Time.deltaTime);
+        //销毁
+        if (transform.position.y<-4.8f) {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    public int hp;
+
+    public float speed;
+
+    public float destroyPosY;
+
+    public int score;
+    // Start is called before the first frame update
+    void Start() {
+        hp = 1;
+        speed = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //运动
+        transform.Translate(Vector3.down*speed*Time.deltaTime);
+        //销毁
+        if (transform.position.y<destroyPosY) {
+            Destroy(this.gameObject);
+        }
+    }
+}
